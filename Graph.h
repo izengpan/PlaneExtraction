d90ed1018d4ddd1504b77fe5ab3145fc16681fcf
Jiang Shengyi <shengyi.jiang@gmail.com>
2016-09-26 21:14:17 +0800

    refractor

--- Graph.h
+++ Graph.h
@@ -13,21 +13,57 @@
 
 using namespace Eigen;
 
-class param {
+
+
+class PlaneParam {
 public:
 	Vector3f normal;
 	float d;
-	param(Vector3f normal=Vector3f(0.0,0.0,0.0), float d=0.0) :normal(normal), d(d) {}
+	PlaneParam(Vector3f normal = Vector3f(0.0, 0.0, 0.0), float d = 0.0) :normal(normal), d(d) {}
 };
+
+class FittingParam {
+public:
+	Vector3f normal;
+	float meanSquareError;
+	FittingParam(Vector3f normal, float meanSqaureError) :normal(normal), meanSquareError(meanSqaureError) {}
+};
+
+FittingParam fitPlane(Matrix3f& const covarianceMatrix) {
+	JacobiSVD<MatrixXf> svd(covarianceMatrix, ComputeThinU | ComputeThinV);
+	Vector3f eigenvalues = svd.singularValues();
+	Matrix3f eigenvectors = svd.matrixU();
+
+	Vector3f normal;
+
+	int minIndex = 0;
+	for (int i = 0; i < eigenvalues.rows(); ++i)
+		if (eigenvalues(i) < eigenvalues(minIndex)) i = minIndex;
+	switch (minIndex) {
+	case 0:
+		normal = eigenvectors.col(1).cross(eigenvectors.col(2));
+		break;
+	case 1:
+		normal = eigenvectors.col(0).cross(eigenvectors.col(2));
+		break;
+	case 2:
+		normal = eigenvectors.col(0).cross(eigenvectors.col(1));
+		break;
+	}
+
+	return FittingParam(normal, eigenvalues(minIndex));
+}
+
 
 
 
 class Node {
 public:
-	Matrix<float, Dynamic, 3, RowMajor> pixels; //(row:(node*node) column:3)
-	//Matrix<int, Dynamic, 2, RowMajor> pixelIndices;
+	Matrix<float,Dynamic, 3, RowMajor> pixels; //(row:(node*node) column:3)
+	
 	std::set<Node**> edges;
-	param planeParam;
+	PlaneParam planeParam;
+	float meanSquareError;
 
 	struct {
 		Matrix3f covarianceMatrix;
@@ -38,12 +74,13 @@ public:
 
 	float meanSquareError = 0;
 
-	Node(float* arr, int length) {
-		if (*arr == NULL || !(length % 3)) return;
-		for (int i = 0; i < length; ++i) {
-			pixels << arr[i];
-			
-		}
+	Node(int* arr=NULL, int x=0,int y=0,int nodesize=0,int row=0,int column=0) {
+		pixels.resize(nodesize*nodesize, 3);
+		if (arr == NULL) return;
+		for (int i = 0; i < nodesize; ++i)
+			for (int j = 0; j < nodesize; ++j) {
+				pixels.row(i) << arr[x*nodesize*column + i*column + y*nodesize + j],arr[x*nodesize*column + i*column + y*nodesize + j + 1],	arr[x*nodesize*column + i*column + y*nodesize + j + 2];
+			}
 		initialization();
 	}
 
@@ -62,17 +99,19 @@ public:
 		}
 	}
 
-	void merge(Node* node) {
-		for (int i = 0; i < node->pixels.rows(); ++i)
-			pixels << node->pixels.row(i);
+	float merge(Node* node) {
+
 		extraData.meanVector = extraData.meanVector*extraData.pixelNumber + node->extraData.meanVector*node->extraData.pixelNumber;
 		extraData.pixelNumber += node->extraData.pixelNumber;
 		extraData.meanVector /= extraData.pixelNumber;
 		extraData.covarianceMatrix += node->extraData.covarianceMatrix;
-
 	}
 
-	void plane();
+	void plane(Matrix3f& const covarianceMatrix) {
+		FittingParam param = fitPlane(covarianceMatrix);
+		meanSquareError = param.meanSquareError;
+		planeParam = PlaneParam(param.normal,param.normal.dot(extraData.meanVector));
+	}
 
 	bool rejectNode();
 
@@ -81,53 +120,35 @@ public:
 	}
 };
 
-
-//use Eigen for matrix operations ;OpenBLAS may be faster
-void Node::plane() {
-	JacobiSVD<MatrixXf> svd(extraData.covarianceMatrix, ComputeThinU | ComputeThinV);
-	Vector3f eigenvalues = svd.singularValues();
-	Matrix3f eigenvectors = svd.matrixU();
-
-	int minIndex = 0;
-	for (int i = 0; i < eigenvalues.rows(); ++i)
-		if (eigenvalues(i) < eigenvalues(minIndex)) i = minIndex;
-	switch (minIndex) {
-	case 0:
-		planeParam.normal = eigenvectors.col(1)*eigenvectors.col(2);
-		break;
-	case 1:
-		planeParam.normal = eigenvectors.col(0)*eigenvectors.col(2);
-		break;
-	case 2:
-		planeParam.normal = eigenvectors.col(0)*eigenvectors.col(1);
-		break;
-	}
-
-	planeParam.d = planeParam.normal.dot(extraData.meanVector);
-}
-
 bool Node::rejectNode() {
+
+
+	//missing data
+	for (int i = 0; i < pixels.rows(); ++i) {
+		if (pixels.row(i)(2) == 0)
+			return true;
+	}
 
 	//depth discountinuity
 	for (int i = 0; i < NODE_SIZE; ++i) 
 		for (int j = 0; j < NODE_SIZE; ++j) {
 			if (i != 0)
-				if (abs(pixels(i*NODE_SIZE + j, 3) - pixels((i - 1)*NODE_SIZE + j, 3)) > 2 * ALPHA*(abs(pixels(i*NODE_SIZE + j, 3)) + 0.5)) return true;
+				if (abs(pixels(i*NODE_SIZE + j, 2) - pixels((i - 1)*NODE_SIZE + j, 2)) > 2 * ALPHA*(abs(pixels(i*NODE_SIZE + j, 2)) + 0.5)) return true;
 			if (j != 0)
-				if (abs(pixels(i*NODE_SIZE + j, 3) - pixels(i*NODE_SIZE + j - 1, 3)) > 2 * ALPHA*(abs(pixels(i*NODE_SIZE + j, 3)) + 0.5)) return true;
+				if (abs(pixels(i*NODE_SIZE + j, 2) - pixels(i*NODE_SIZE + j - 1, 2)) > 2 * ALPHA*(abs(pixels(i*NODE_SIZE + j, 2)) + 0.5)) return true;
 			if (i != NODE_SIZE - 1)
-				if (abs(pixels(i*NODE_SIZE + j, 3) - pixels((i + 1)*NODE_SIZE + j, 3)) > 2 * ALPHA*(abs(pixels(i*NODE_SIZE + j, 3)) + 0.5)) return true;
+				if (abs(pixels(i*NODE_SIZE + j, 2) - pixels((i + 1)*NODE_SIZE + j, 2)) > 2 * ALPHA*(abs(pixels(i*NODE_SIZE + j, 2)) + 0.5)) return true;
 			if (j != NODE_SIZE - 1)
-				if (abs(pixels(i*NODE_SIZE + j, 3) - pixels(i *NODE_SIZE + j + 1, 3)) > 2 * ALPHA*(abs(pixels(i*NODE_SIZE + j, 3)) + 0.5)) return true;
+				if (abs(pixels(i*NODE_SIZE + j, 2) - pixels(i *NODE_SIZE + j + 1, 2)) > 2 * ALPHA*(abs(pixels(i*NODE_SIZE + j, 2)) + 0.5)) return true;
 		}
 
 	//over-MSE
-	plane();
-	for (int i = 0; i < pixels.rows; i++) {
-		meanSquareError += pow(planeParam.normal.dot(pixels.row(i)), 2);
-	}
+	plane(extraData.covarianceMatrix);
+	
 	if (meanSquareError > pow(((SIGMA*extraData.meanVector(2)*extraData.meanVector(2) + EPSILON)), 2))
 		return true;
+	
+	pixels.resize(0, 0);
 
 	return false;
 }
@@ -136,7 +157,13 @@ class Graph {
 public:
 	int row, column;
 	std::vector<Node*> graph;
+
+	Graph(int row, int column) :row(row), column(column) {}
+
 	void connectEdge();
+	void addNode(Node* node) {
+		graph.push_back(node);
+	}
 };
 
 //¶þ¼¶Ö¸Õë
@@ -144,25 +171,25 @@ public:
 void Graph::connectEdge() {
 	for (int i = 0; i < row; i++)
 		for (int j = 0; j < column; j++) {
-			if (graph[i*row + j] == NULL)
+			if (graph[i*column + j] == NULL)
 				continue;
 			if (i != 0 && i != row-1) {
-				if (graph[(i - 1)*row + j] != NULL&&graph[(i + 1)*row + j] != NULL)
-					if (graph[(i - 1)*row + j]->planeParam.normal.dot(graph[(i + 1)*row + j]->planeParam.normal) > T_ANG) {
-						graph[i*row + j]->edges.insert(&graph[(i - 1)*row + j]);
-						graph[i*row + j]->edges.insert(&graph[(i + 1)*row + j]);
-						graph[(i - 1)*row + j]->edges.insert(&graph[i*row + j]);
-						graph[(i + 1)*row + j]->edges.insert(&graph[i*row + j]);
+				if (graph[(i - 1)*column + j] != NULL&&graph[(i + 1)*column + j] != NULL)
+					if (graph[(i - 1)*column + j]->planeParam.normal.dot(graph[(i + 1)*column + j]->planeParam.normal) > T_ANG) {
+						graph[i*column + j]->edges.insert(&graph[(i - 1)*column + j]);
+						graph[i*column + j]->edges.insert(&graph[(i + 1)*column + j]);
+						graph[(i - 1)*column + j]->edges.insert(&graph[i*column + j]);
+						graph[(i + 1)*column + j]->edges.insert(&graph[i*column + j]);
 					}
 			}
 			if (j != 0 && j != column - 1) {
-				if (graph[i*row + j - 1] != NULL&&graph[i*row + j + 1] != NULL)
-					if (graph[i*row + j - 1]->planeParam.normal.dot(graph[i*row + (j + 1)]->planeParam.normal) > T_ANG)
+				if (graph[i*column + j - 1] != NULL&&graph[i*column + j + 1] != NULL)
+					if (graph[i*column + j - 1]->planeParam.normal.dot(graph[i*column + (j + 1)]->planeParam.normal) > T_ANG)
 					{
-						graph[i*row + j]->edges.insert(&graph[i*row + j - 1]);
-						graph[i*row + j]->edges.insert(&graph[i*row + j + 1]);
-						graph[i*row + j - 1]->edges.insert(&graph[i*row + j]);
-						graph[i*row + j + 1]->edges.insert(&graph[i*row + j]);
+						graph[i*column + j]->edges.insert(&graph[i*column + j - 1]);
+						graph[i*column + j]->edges.insert(&graph[i*column + j + 1]);
+						graph[i*column + j - 1]->edges.insert(&graph[i*column + j]);
+						graph[i*column + j + 1]->edges.insert(&graph[i*column + j]);
 					}
 			}
 		}
--- PlaneExtraction.cpp
+++ PlaneExtraction.cpp
@@ -1,9 +1,11 @@
 #define T_NUM 800
+
 #include <queue>
 #include "Graph.h"
-
+#include <fstream>
 
 std::vector<Node*> coarseResult;
+
 
 
 void AHCluster(Graph* graph) {
@@ -16,8 +18,8 @@ void AHCluster(Graph* graph) {
 		queue.pop();
 		if ((*node) == NULL) continue;
 		float minMSE=-1.0; 
-		Node** bestNode;
-		Node* mergeNode;
+		Node** bestNode=NULL;
+		Node* mergeNode=NULL;
 		for (auto edge : (*node)->edges) {
 			Node* currentNode = *edge;
 			float currentMSE;
@@ -53,7 +55,40 @@ void AHCluster(Graph* graph) {
 
 }
 
+void refine(Graph* graph) {
+
+
+}
+
+
 int main() {
+	std::ifstream os;
+	os.open("depth.txt");
+	int row;
+	int column;
+	int* data;
+	os >> row>>column;
+	data = new int[row*column * 3];
+	
+	int k = 0;
+	for (int i = 0; i < row; ++i) 
+		for (int j = 0; j < column; ++j) {
+			int d;
+			os >> d;
+			data[k++] = (float)(i - row / 2) /528 * d;
+			data[k++] = (float)(j - column / 2) / 528 * d;
+			data[k++] = d;
+		}
+	Graph rawdata(row / NODE_SIZE, column / NODE_SIZE);
+
+	for (int i = 0; i < row / NODE_SIZE; ++i) 
+		for (int j = 0; j < column / NODE_SIZE; ++j) {
+			Node* currentNode = new Node(data, i, j, NODE_SIZE, row, column);
+			if (currentNode->rejectNode()) rawdata.addNode(NULL);
+			else rawdata.addNode(currentNode);
+		
+		}
+	rawdata.connectEdge();
 
 	return 0;
 }
